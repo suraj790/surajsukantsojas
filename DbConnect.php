2d688b0bda27d65fb5d230c18f0a6822b2376310
suraj790 <66895098+suraj790@users.noreply.github.com>
2020-06-19 08:40:14 +0530

    Update DbConnect.php

--- DbConnect.php
+++ DbConnect.php
@@ -1,13 +1,19 @@
 <?php 
 	class DbConnect {
-		private $host = 'localhost';
-		private $dbName = 'cloud';
-		private $user = 'root';
-		private $pass = '';
+		private $host;
+		private $dbName;
+		private $user;
+		private $pass;
 
+		public function __construct() {
+			$this->host = getenv( varname: 'MYSQL_HOST');
+			$this->dbname = getenv( varname: 'MYSQL_DB');
+			$this->user = getenv( varname: 'MYSQL_USER');
+			$this->pass = getenv( varname: 'MYSQL_PASS');
+		}
 		public function connect() {
 			try {
-				$conn = new PDO('mysql:host=' . $this->host . '; dbname=' . $this->dbName, $this->user, $this->pass);
+				$conn = new PDO($this->host . '; dbname=' . $this->dbName, $this->user, $this->pass);
 				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				return $conn;
 			} catch( PDOException $e) {
